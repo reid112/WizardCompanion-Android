2c68998ad9c2420f313628749ca1d418949a0c84
Reid <rreid112@gmail.com>
2022-03-16 16:41:03 -0600

    Removed todo

--- app/src/main/java/ca/rjreid/wizardcompanion/ui/theme/Type.kt
+++ app/src/main/java/ca/rjreid/wizardcompanion/ui/theme/Type.kt
@@ -2,11 +2,9 @@ package ca.rjreid.wizardcompanion.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// TODO: Customize this
 val Typography = Typography(
     h1 = TextStyle(
         fontWeight = FontWeight.Light,
