4cf90c1d5ca1e4f6285d6d318896570e4702b228
Reid <rreid112@gmail.com>
2022-03-27 05:05:03 -0600

    WIP - settings screen

--- app/src/main/java/ca/rjreid/wizardcompanion/data/models/Settings.kt
+++ app/src/main/java/ca/rjreid/wizardcompanion/data/models/Settings.kt
@@ -1,9 +1,11 @@
 package ca.rjreid.wizardcompanion.data.models
 
-enum class ThemeSetting(val value: Int) {
-    LIGHT(1),
-    DARK(2),
-    SYSTEM(3);
+import ca.rjreid.wizardcompanion.R
+
+enum class ThemeSetting(val value: Int, val stringResId: Int) {
+    LIGHT(1, R.string.enum_theme_light),
+    DARK(2, R.string.enum_theme_dark),
+    SYSTEM(3, R.string.enum_theme_system);
 
     companion object {
         private val map = values().associateBy(ThemeSetting::value)
--- app/src/main/java/ca/rjreid/wizardcompanion/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/ca/rjreid/wizardcompanion/ui/screens/settings/SettingsScreen.kt
@@ -1,23 +1,80 @@
 package ca.rjreid.wizardcompanion.ui.screens.settings
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import ca.rjreid.wizardcompanion.R
+import ca.rjreid.wizardcompanion.data.models.ThemeSetting
+import ca.rjreid.wizardcompanion.ui.theme.WizardCompanionTheme
+import ca.rjreid.wizardcompanion.ui.theme.spacing
 
 @Composable
 fun SettingsScreen(
     viewModel: SettingsViewModel = hiltViewModel()
 ) {
     val uiState = viewModel.uiState
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)
-    ) {
 
+    SettingsCard(uiState, viewModel::toggleScreen)
+}
+
+@Composable
+fun SettingsCard(
+    state: UiState,
+    onThemeRowClicked: () -> Unit = {},
+    onChangeTheme: (ThemeSetting) -> Unit = {},
+    onToggleScreen: () -> Unit = {}
+) {
+    Card(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.padding(MaterialTheme.spacing.medium)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(MaterialTheme.spacing.extraLarge)
+                    .clickable { onThemeRowClicked() },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(text = stringResource(R.string.setting_theme))
+                Text(
+                    text = stringResource(state.theme.stringResId),
+                    style = MaterialTheme.typography.caption
+                )
+            }
+            Divider()
+            Row(
+                modifier = Modifier.fillMaxWidth().height(MaterialTheme.spacing.extraLarge),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = stringResource(R.string.setting_keep_screen_on))
+                Switch(
+                    checked = state.keepScreenOn,
+                    onCheckedChange = { onToggleScreen() }
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SettingsCardPreview() {
+    WizardCompanionTheme {
+        SettingsCard(state = UiState())
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun SettingsCardDarkPreview() {
+    WizardCompanionTheme {
+        SettingsCard(state = UiState())
     }
 }
