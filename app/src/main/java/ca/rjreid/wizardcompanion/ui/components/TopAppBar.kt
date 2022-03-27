4430b0b14ee022fcb53094a1e0ea5ceaeaa77a5b
Reid <rreid112@gmail.com>
2022-03-27 06:58:25 -0600

    Added nav icon for a few screens

--- app/src/main/java/ca/rjreid/wizardcompanion/ui/components/TopAppBar.kt
+++ app/src/main/java/ca/rjreid/wizardcompanion/ui/components/TopAppBar.kt
@@ -1,9 +1,8 @@
 package ca.rjreid.wizardcompanion.ui.components
 
-import androidx.compose.material.AppBarDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -18,25 +17,72 @@ fun TopAppBar(navController: NavController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     var elevation by remember { mutableStateOf(AppBarDefaults.TopAppBarElevation) }
     var currentTitle by remember { mutableStateOf("") }
+    var navIcon by remember { mutableStateOf<AppBarIcon?>(null)}
 
     when(navBackStackEntry?.destination?.route) {
-        Screen.Home.route -> currentTitle = stringResource(id = R.string.screen_title_home)
-        Screen.PastGames.route -> currentTitle = stringResource(id = R.string.screen_title_past_games)
-        Screen.Settings.route -> currentTitle = stringResource(id = R.string.screen_title_settings)
-        Screen.EnterPlayers.route -> currentTitle = stringResource(id = R.string.screen_title_enter_players)
+        Screen.Home.route -> {
+            currentTitle = stringResource(id = R.string.screen_title_home)
+            elevation = AppBarDefaults.TopAppBarElevation
+            navIcon = null
+        }
+        Screen.PastGames.route -> {
+            currentTitle = stringResource(id = R.string.screen_title_past_games)
+            elevation = AppBarDefaults.TopAppBarElevation
+            navIcon = null
+        }
+        Screen.Settings.route -> {
+            currentTitle = stringResource(id = R.string.screen_title_settings)
+            elevation = AppBarDefaults.TopAppBarElevation
+            navIcon = null
+        }
+        Screen.EnterPlayers.route -> {
+            currentTitle = stringResource(id = R.string.screen_title_enter_players)
+            elevation = AppBarDefaults.TopAppBarElevation
+            navIcon = AppBarIcon.BACK
+        }
         Screen.Score.route -> {
             currentTitle = stringResource(id = R.string.screen_title_score)
             elevation = 0.dp
+            navIcon = AppBarIcon.BACK
         }
-        Screen.GameDetails.route -> currentTitle = stringResource(id = R.string.screen_title_game_details)
-        else -> currentTitle = stringResource(id = R.string.app_name)
+        Screen.GameDetails.route -> {
+            currentTitle = stringResource(id = R.string.screen_title_game_details)
+            elevation = AppBarDefaults.TopAppBarElevation
+            navIcon = AppBarIcon.BACK
+        }
+        else -> {
+            currentTitle = stringResource(id = R.string.app_name)
+            elevation = AppBarDefaults.TopAppBarElevation
+            navIcon = null
+        }
     }
 
     TopAppBar(
         title = { Text(text = currentTitle) },
-        navigationIcon = null,
+        navigationIcon = navIcon?.let { { NavIcon(navController, it) } },
         backgroundColor = MaterialTheme.colors.primary,
         contentColor = MaterialTheme.colors.onPrimary,
         elevation = elevation
     )
+}
+
+@Composable
+fun NavIcon(
+    navController: NavController,
+    navIcon: AppBarIcon
+) {
+    when (navIcon) {
+        AppBarIcon.BACK -> {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = stringResource(id = R.string.content_description_back)
+                )
+            }
+        }
+    }
+}
+
+enum class AppBarIcon {
+    BACK
 }
